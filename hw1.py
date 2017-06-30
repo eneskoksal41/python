a218e37a15a4d611b694622a8a61cfec4f78d276
enes <koksal.enes.41@gmail.com>
2017-07-01 02:48:43 +0300

    Son

--- hw1.py
+++ hw1.py
@@ -1,1 +1,28 @@
+def topla(a,b):
+    return a+b
 print ("hello world")
+isim = "Ahmet"
+print(isim)
+fiyat = 3
+fiyat += fiyat*0.2
+
+print(fiyat)
+
+liste = [3,5,6]
+print(liste[1:-2])
+
+tup = (22,174, 74.5 ,"ahmet")
+
+dic = {"ahmet":21,"mehmet":23}
+for i in liste:
+    if i == 5:
+        print("adasdasd")
+    elif i == 6:
+        print("altı")
+    else:
+        print("üçç")
+    print(i)
+
+print(topla(10,10))
+
+
